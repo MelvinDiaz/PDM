5585c7fc6731dd63c69a3d9a25bbae5ff75c6a4f
MelvinDiaz <00008821@uca.edu.sv>
2023-03-10 15:31:02 -0600

    casi finalizado

--- EjercicioClase01/app/src/main/java/com/example/ejercicioclase/MainActivity.kt
+++ EjercicioClase01/app/src/main/java/com/example/ejercicioclase/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.ejercicioclase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+
     }
-}
+        fun bindViews() {
+            val btn = findViewById<Button>(R.id.mySendButton)
+
+            btn.setOnClickListener {
+                val intent = Intent(this, SecondActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
